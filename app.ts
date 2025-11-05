12f770886d5821b1d82fa817c29f648a820ab3b8
Soumyaranjan-p <ranjanparidasoumya04@gmail.com>
2025-11-05 22:28:31 +0530

    updated

--- app.ts
+++ app.ts
@@ -1,5 +1,40 @@
-const greet = (name: string): string => {
-  return `Hello, ${name}!`;
-};
+// const greet = (name: string): string => {
+//   return `Hello, ${name}!`;
+// };
 
-console.log(greet("Soumya"));
+// console.log(greet("Soumya"));
+
+
+//BASIC TYPES
+//PRIMITIVE TYPES
+//ARRAYS
+//.TUPLES
+// .ENUMS
+//. ANY,UNKNOWN,VOID, NULL ,UNDEFINED,NEVER
+
+
+// Primitive and reference
+
+//var a =12; //---->>PRIMITIVE
+// [],{},       // REFERENCE
+
+//----let a: number = 10;
+//------let b = a;   // b gets a copy of the value (10)
+//----b = 20;      // changing b does NOT affect a
+
+//   console.log(a); // 10
+// ----console.log(b); // 20
+
+// ---Here, a and b are independent — each holds its own copy of the number.
+
+
+        //REFERENCE------------------->>>>>>>>>>>
+
+
+        let obj1 = { name: "Alice", age: 25 };
+let obj2 = obj1; // obj2 references the same object in memory
+
+obj2.age = 30;
+
+console.log(obj1.age); //--------> 30
+console.log(obj2.age); //--------> 30
