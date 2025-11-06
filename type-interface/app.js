a35735b0ed2331890742ce3b4ed01b101737b76e
Soumyaranjan-p <ranjanparidasoumya04@gmail.com>
2025-11-06 17:12:08 +0530

    add initial TypeScript files with User and Admin interfaces

---
+++ type-interface/app.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function abcd(obj) {
+}
+abcd({ name: "hey", email: "gey@gmail.com", password: "123" });
+function abcd(obj) {
+}
+abcd({ name: "hey", email: "gey@gmail.com", password: "123" });
+//---TYPE - ALIASES
+//# sourceMappingURL=app.js.map
