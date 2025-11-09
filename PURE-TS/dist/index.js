8e7adbe7609a9c872add4f9009d9e6020a16545a
Soumyaranjan-p <ranjanparidasoumya04@gmail.com>
2025-11-09 19:20:00 +0530

    added getter and setter

--- PURE-TS/dist/index.js
+++ PURE-TS/dist/index.js
@@ -1,3 +1,45 @@
-console.log("typescript is here--stll");
-export {};
+"use strict";
+// class User {
+//     public email: string
+//     private name: string
+//     readonly city: string = "Jaipur"
+//     constructor(email: string, name: string){
+//         this.email = email;
+//         this.name = name
+//     }
+// }
+class User {
+    email;
+    name;
+    _courseCount = 1;
+    city = "Cuttack";
+    constructor(email, name) {
+        this.email = email;
+        this.name = name;
+    }
+    deleteToken() {
+        console.log("Token deleted");
+    }
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+    get courseCount() {
+        return this._courseCount;
+    }
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = courseNum;
+    }
+}
+class SubUser extends User {
+    isFamily = true;
+    changeCourseCount() {
+        this._courseCount = 4;
+    }
+}
+const hitesh = new User("h@h.com", "hitesh");
+// hitesh.name 
+// hitesh.deleteToken()
 //# sourceMappingURL=index.js.map
