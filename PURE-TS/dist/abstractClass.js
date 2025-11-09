d08251e1e5aefa616e075771ba99b9b4e995fe8e
Soumyaranjan-p <ranjanparidasoumya04@gmail.com>
2025-11-09 19:42:52 +0530

    added abstact class

---
+++ PURE-TS/dist/abstractClass.js
@@ -0,0 +1,30 @@
+"use strict";
+class TakePhoto {
+    cameraMode;
+    filter;
+    constructor(cameraMode, filter) {
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+    }
+    getReelTime() {
+        //some complex calculation
+        return 8;
+    }
+}
+class Instagram extends TakePhoto {
+    cameraMode;
+    filter;
+    burst;
+    constructor(cameraMode, filter, burst) {
+        super(cameraMode, filter);
+        this.cameraMode = cameraMode;
+        this.filter = filter;
+        this.burst = burst;
+    }
+    getSepia() {
+        console.log("Sepia");
+    }
+}
+const hc = new Instagram("test", "Test", 3);
+hc.getReelTime();
+//# sourceMappingURL=abstractClass.js.map
