c55d5cb9a1c08925383971e311c3dbce98c00270
Soumyaranjan-p <ranjanparidasoumya04@gmail.com>
2025-11-16 00:12:11 +0530

    added functions

---
+++ FUNCTIONS/app.ts
@@ -0,0 +1,27 @@
+function add(x: number, y: number): number {
+  return x + y;
+}
+ 
+// let myAdd = function (x: number, y: number): number {
+//   return x + y;
+// };
+
+// let myAdd: (baseValue: number, increment: number) => number = function (
+//   x: number,
+//   y: number
+// ): number {
+//   return x + y;
+// };
+
+
+   
+
+function abcd(name:string,age:number,cb:(arg:string)=>void){
+    cb("hey")
+   }  
+
+   abcd ("hey",56, (arg:string)=>{
+    console.log("hey");
+   })
+
+   
